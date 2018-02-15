1f9deb0d4e673f77aa684e617df171fde0854611
Malarvizhiparvathi <36284938+Malarvizhiparvathi@users.noreply.github.com>
2018-02-15 02:12:37 -0800

    Update leapyear.py

--- leapyear.py
+++ leapyear.py
@@ -1,9 +1,5 @@
-a=raw_input("")
-
+a=int(input(""))
 if(a%4==0):
-    
-   print("a is a leap year")
-
+    print("a is a leap year")
 else:
-    
-   print("a is not a leap year")
+    print("a is not a leap year")
